5960cc32c3cd65c8915726a0276d7c8347c59322
HirofumiOkawachig2 <43422473+HirofumiOkawachig2@users.noreply.github.com>
2018-09-23 07:53:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-header("Location: https://www.google.com/aclk?sa=L&ai=DChcSEwjM2o7o6MfdAhWB42QKHa5UA_8YABAAGgJwag&sig=AOD64_2L6l3nXXOW17qzWJpidtXoto8iig&ved=2ahUKEwiGr4jo6MfdAhV0OX0KHRPuBWYQjhp6BAgJEAE&adurl=");
+header("Location: https://www.centralglassandmirror.com/
+");
 die();
 ?>
